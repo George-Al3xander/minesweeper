da678a45f8a4d40d72eec05a2eb3db44791faa4b
George <george.valuiskyi@gmail.com>
2025-04-15 13:57:28 +0300

    build: clean up
    Remove unused files and simplify boilerplate files.

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="mx-auto w-[min(90%,20rem)] p-4">
+            <p className="text-gray-600">Text</p>
+        </div>
+    );
+};
+
+export default App;
