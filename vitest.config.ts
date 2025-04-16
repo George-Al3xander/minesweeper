c4532bc6817b088c72a86ad1a7e6aed2645fd52a
George <george.valuiskyi@gmail.com>
2025-04-16 12:00:14 +0300

    test: set up vitest
    Install and configure Vitest.

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import * as path from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
