3ad9e074270a8793448c55a9867fc6ddbdd61210
George <george.valuiskyi@gmail.com>
2025-04-29 18:23:11 +0300

    feat: cell content
    Create the CellContent component. Add CSS variables for the following
    colors: cell's primary, secondary, and accent; 1-8 number colors for
    Minesweeper. Implement CSS classes for the three-dimensional effect and
    cell.

---
+++ src/app/components/cell-content.tsx
@@ -0,0 +1,59 @@
+import { TCell } from "@/types/models/cell";
+import { FlagIcon, MineIcon } from "@/ui/icons";
+import { cn } from "@/utils/cn";
+import { FC, memo, ReactNode } from "react";
+
+type Props = TCell;
+
+const colorKeys = [
+    "text-one",
+    "text-one",
+    "text-two",
+    "text-three",
+    "text-four",
+    "text-five",
+    "text-six",
+    "text-seven",
+    "text-eight",
+];
+
+export const CellContent: FC<Props> = memo(
+    ({
+        hasMine,
+        isFlagged,
+        isOpen,
+
+        neighborMinesCount,
+    }) => {
+        let content: ReactNode = null;
+
+        if (isOpen) {
+            if (hasMine) {
+                content = <MineIcon size={18} className="text-black" />;
+            } else if (neighborMinesCount > 0) {
+                content = (
+                    <span className={colorKeys[neighborMinesCount]}>
+                        {neighborMinesCount}
+                    </span>
+                );
+            }
+        } else if (isFlagged) {
+            content = <FlagIcon className="text-red-600" />;
+        }
+
+        return (
+            <span
+                className={cn(
+                    "bg-primary cell absolute inset-0 flex size-full items-center justify-center",
+                    {
+                        "cell-closed three-d-borders": !isOpen,
+                    },
+                )}
+            >
+                {content}
+            </span>
+        );
+    },
+    (curr, next) =>
+        curr.isOpen === next.isOpen && curr.isFlagged === next.isFlagged,
+);
